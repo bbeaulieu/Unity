8106ca49b0a1ea154f92c216a7c23252b9d34a04
brad <brad.j.beaulieu@gmail.com>
2016-10-25 21:43:58 -0400

    first commit

---
+++ survival game/Assets/Scripts/EnemyLogic.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+var Health = 100;
+
+function Update () {
+	if (Health <= 0){
+		Dead();
+	}
+}
+
+function ApplyDamage(PlayerDamage : int) {
+	Health -= PlayerDamage;
+
+}
+
+function Dead () {
+	Destroy (gameObject);
+}
